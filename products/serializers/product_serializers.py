97ffa40034e39a1dd3ede2c604b56dcf48297278
muhammedshamilv <shamil@hamon.in>
2024-01-09 11:33:19 +0530

    adds representation on cart and order serializer

--- products/serializers/product_serializers.py
+++ products/serializers/product_serializers.py
@@ -3,10 +3,15 @@ from products.models import *
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    image=serializers.SerializerMethodField()
     class Meta:
         model = Product
         fields = "__all__"
-
+        read_only_field=("name","price","details","availability","image","category")
+    def get_image(self,instance):
+        if instance.image:
+            return "http://127.0.0.1:8000"+instance.image.url 
+        return "    " 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -17,13 +22,23 @@ class CategorySerializer(serializers.ModelSerializer):
 class CartSerializer(serializers.ModelSerializer):
     # user=serializers.UUIDField()
     # product=serializers.UUIDField()
-
+    
     class Meta:
         model = Cart
         fields = "__all__"
 
+    def to_representation(self, instance):
+        rep= super().to_representation(instance)
+        rep["product"]=ProductSerializer(instance.product).data
+        return rep
+    
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = "__all__"
+
+    def to_representation(self, instance):
+        rep= super().to_representation(instance)
+        rep["product"]=ProductSerializer(instance.product).data
+        return rep
